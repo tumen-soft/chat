a9134db97647c4487e86761ab2fa0ebd8eb33e83
Denis Vergun <blu.eagle@mail.ru>
2022-06-17 12:01:57 +0500

    client fucktion

--- chat.c
+++ chat.c
@@ -101,14 +101,87 @@ ServerCtr(Server * const me){
 arg * pclient;
 
 //typedef struct {Server server; Client client;}Self;
-void ConnectServer(){printf("starting server at ...(ip)\n");}
+void ConnectServer(Server * const me){
+printf("starting server at ...(ip)\n");
+
+
+me->max_clients=10;
+me->sd=0;
+me->sd2=0;
+        check(bind(me->peer.sock, (struct sockaddr *)&me->peer.addres, sizeof(me->peer.addres)), "error bind"    );
+        check(listen(me->peer.sock, 15), "error listen");
+        for (int i = 0; i < me->max_clients; i++) me->client_socket[i] = -1;  
+
+/*
+       while(CHAT){
+        FD_ZERO(&ser.peer.read_fd);      
+        ser.max_sd = ser.peer.sock; 
+	ser.client_socket[0]=0;
+	for (int i = 0 ; i < ser.max_clients ; i++){ 
+        ser.sd = ser.client_socket[i]; 
+        if(ser.sd >= 0)FD_SET( ser.sd , &ser.peer.read_fd); 
+        if(ser.sd > ser.max_sd)ser.max_sd = ser.sd;}  
+        //FD_SET(0, &ser.peer.read_fd);
+        FD_SET(ser.peer.sock, &ser.peer.read_fd);  
+        select(300, &ser.peer.read_fd, NULL, NULL, NULL);
+	if (FD_ISSET(ser.peer.sock, &ser.peer.read_fd)){ 
+	ser.new_socket = accept(ser.peer.sock,NULL,NULL);
+	ser.new_socket = accept(ser.peer.sock,NULL,NULL);	
+	printf("New connection %d\n",ser.new_socket);	
+	dprintf(ser.new_socket,"welcome %d\n",ser.new_socket);	
+	puts("Welcome message sent."); 
+	for (int j = 0; j < ser.max_clients; j++) 
+	{ 
+	if( ser.client_socket[j] == -1) 
+	{ 
+	ser.client_socket[j] = ser.new_socket; 
+	printf("Adding to list of sockets as %d\n" , j); 
+	break; 
+	} 
+	} 
+	} 
+
+		for (int k = 0; k < ser.max_clients; k++) 
+		{ 
+			//ser.sd = ser.client_socket[k]; 
+			if (FD_ISSET( ser.client_socket[k] , &ser.peer.read_fd )) 
+			{ 
+				if ((ser.valread = read(ser.client_socket[k], &ser.peer.buffer, sizeof(ser.peer.buffer))) == 0)//man read 
+				{ 
+				
+					printf("Host disconnected %d \n" , ser.client_socket[k]); 
+					close( ser.client_socket[k] ); 
+					ser.client_socket[k] = -1; 
+				} 
+			else
+				{ 
+			//ser.peer.buffer[ser.valread] = '\0';  
+         		for ( int l = 0 ; l < ser.max_clients ; l++){if(ser.client_socket[l]>=0)dprintf(ser.client_socket[l],"%i says: %s\n",ser.client_socket[k], ser.peer.buffer);
+
+			//printf("%i says: %s\n",ser.client_socket[k],ser.peer.buffer);}
+		        //if (l==ser.max_clients-1)printf("%s\n",ser.peer.buffer);
+}
+				}
+			} 
+		} 
+	}
+	close(ser.sd2);
+	close(ser.peer.sock);
+
+}
+}
+*/
+return 0;
+}
+
+
 void ConnectClient(Client * const me){
 printf("starting client at ...(ip)\n");
 pclient->ip.s_addr=inet_addr(pclient->y);
 me->peer.addres.sin_addr = pclient->ip;
 //me->peer.addres.sin_addr = pclient->ip.s_addr++;
 //me->peer.addres.sin_addr = pclient->ip.s_addr--;
-pclient->ip.s_addr  = htonl(ntohl(pclient->ip.s_addr)+1);
+//pclient->ip.s_addr  = htonl(ntohl(pclient->ip.s_addr)+1);
 
 connect(me->peer.sock, (struct sockaddr*)&me->peer.addres, sizeof(me->peer.addres));
         while(CHAT){
@@ -276,28 +349,7 @@ switch(isserver)
 //Client cli;
 ClientCtr(&cli);
 ConnectToChat(&cli);
-//cli.peer.addres.sin_addr.s_addr = inet_addr(pclient->y);
-//connect(cli.peer.sock, (struct sockaddr*)&cli.peer.addres, sizeof(cli.peer.addres));
-	//cli.peer.sock=socke;
-	//read(cli.peer.sock, cli.peer.buffer, sizeof(cli.peer.buffer));
-        //printf("server: %s\n",cli.peer.buffer);
-	/*for(;;){
-	memset(cli.peer.buffer, 0, sizeof(cli.peer.buffer));
-	FD_ZERO(&cli.peer.read_fd);
-        FD_SET(0, &cli.peer.read_fd);
-        FD_SET(cli.peer.sock, &cli.peer.read_fd);
-        select(300, &cli.peer.read_fd, NULL, NULL, NULL);
-	if(FD_ISSET(0, &cli.peer.read_fd)){
-	read(0,cli.peer.buffer,sizeof(cli.peer.buffer));
-	dprintf(cli.peer.sock,cli.peer.buffer);}  
-        if(FD_ISSET(cli.peer.sock, &cli.peer.read_fd)){
-	read(cli.peer.sock, cli.peer.buffer, sizeof(cli.peer.buffer));
-	printf("server: %s\n", cli.peer.buffer);}
-	}        
-	close(cli.peer.sock);
-
-
-return 0;*/
+return 0;
 }
 
 
@@ -311,13 +363,13 @@ ServerCtr(&ser);
 ConnectToChat(&ser);
 
 
-ser.max_clients=10;
+/*ser.max_clients=10;
 ser.sd=0;
 ser.sd2=0;
         check(bind(ser.peer.sock, (struct sockaddr *)&ser.peer.addres, sizeof(ser.peer.addres)), "error bind"    );
         check(listen(ser.peer.sock, 15), "error listen");
         for (int i = 0; i < ser.max_clients; i++) ser.client_socket[i] = -1;  
-
+*/
         while(CHAT){
         FD_ZERO(&ser.peer.read_fd);      
         ser.max_sd = ser.peer.sock; 
