a5c185200338f452781af52d155ad0c9f14dc373
Denis Vergun <blu.eagle@mail.ru>
2022-11-21 16:52:17 +0500

    typelist

---
+++ cpp-typelist/CMakeFiles/run_tests.dir/tests.cpp.o.d
@@ -0,0 +1,39 @@
+CMakeFiles/run_tests.dir/tests.cpp.o: \
+ /root/container/cpp-typelist/tests.cpp /usr/include/stdc-predef.h \
+ /usr/include/c++/11.2.0/type_traits \
+ /usr/include/c++/11.2.0/x86_64-pc-linux-gnu/bits/c++config.h \
+ /usr/include/c++/11.2.0/x86_64-pc-linux-gnu/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/c++/11.2.0/x86_64-pc-linux-gnu/bits/cpu_defines.h \
+ /root/container/cpp-typelist/typelist.hpp \
+ /usr/include/c++/11.2.0/cstdint \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.2.0/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/libc-header-start.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/time64.h /usr/include/bits/wchar.h \
+ /usr/include/bits/stdint-intn.h /usr/include/bits/stdint-uintn.h \
+ /usr/include/c++/11.2.0/tuple /usr/include/c++/11.2.0/utility \
+ /usr/include/c++/11.2.0/bits/stl_relops.h \
+ /usr/include/c++/11.2.0/bits/stl_pair.h \
+ /usr/include/c++/11.2.0/bits/move.h \
+ /usr/include/c++/11.2.0/initializer_list /usr/include/c++/11.2.0/array \
+ /usr/include/c++/11.2.0/bits/functexcept.h \
+ /usr/include/c++/11.2.0/bits/exception_defines.h \
+ /usr/include/c++/11.2.0/bits/stl_algobase.h \
+ /usr/include/c++/11.2.0/bits/cpp_type_traits.h \
+ /usr/include/c++/11.2.0/ext/type_traits.h \
+ /usr/include/c++/11.2.0/ext/numeric_traits.h \
+ /usr/include/c++/11.2.0/bits/stl_iterator_base_types.h \
+ /usr/include/c++/11.2.0/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/11.2.0/bits/concept_check.h \
+ /usr/include/c++/11.2.0/debug/assertions.h \
+ /usr/include/c++/11.2.0/bits/stl_iterator.h \
+ /usr/include/c++/11.2.0/bits/ptr_traits.h \
+ /usr/include/c++/11.2.0/debug/debug.h \
+ /usr/include/c++/11.2.0/bits/predefined_ops.h \
+ /usr/include/c++/11.2.0/bits/range_access.h \
+ /usr/include/c++/11.2.0/bits/uses_allocator.h \
+ /usr/include/c++/11.2.0/bits/invoke.h
