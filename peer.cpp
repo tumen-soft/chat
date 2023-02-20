3de19782dce760c9826c758d0ed495bbde282521
Denis Vergun <blu.eagle@mail.ru>
2023-02-20 14:39:48 +0500

    main funct

--- peer.cpp
+++ peer.cpp
@@ -66,101 +66,6 @@
 #include <peer.h>
 
 
-void Server::r()
-{
-	//std::cout<<c<<std::endl;
-	//socket creation
-        sock=socket(AF_INET, SOCK_STREAM, 0);
-	//output socket file descriptor
-	printf("server fd %i \n", sock);
-	//initialisation
-        bind(sock, (struct sockaddr*)&addres, sizeof(addres));
-	//waiting for connection
-        listen(sock, 300);
-
-	nicknames.insert({0,s});
-	
-        for(;;){
-        FD_ZERO(&read_fd);      
-        //comm->max_sd = comm->sock; 
-      //  for (auto it1 = self->nicknames.begin(); it1!=self->nicknames.end();  ++it1)
-    //std::cout << it1->first << "->" << it1->second << std::endl;  
-	for (auto itr = nicknames.begin(); itr != nicknames.end(); ++itr)FD_SET(itr->first, &read_fd);
-        FD_SET(sock, &read_fd);  
-	//wait for connection/recive message
-        select(300, &read_fd, NULL, NULL, NULL);
-	
-	if (FD_ISSET(sock, &read_fd)) 
-                {	
-			//furst for scanner	
-			new_socket = accept(sock,NULL,NULL);
-			//accepting connection
-			new_socket = accept(sock,NULL,NULL);
-			char g[80]={0};
-			read(new_socket,g,1024);
-			nicknames.insert({new_socket,g});
-			printf("New connection %s\n", nicknames.find(new_socket)->second);
-			dprintf(new_socket,"welcome %d\n", new_socket);  
-                }
-		//for (auto it2 = self->nicknames.begin(); it2!=self->nicknames.end();  ++it2)
-    //std::cout << it2->first << "->" << it2->second << std::endl; 
-		for (auto itr2 = nicknames.begin(); itr2 != nicknames.end(); ++itr2)
-                { 
-			sd = itr2->first; 
-			if (FD_ISSET(sd , &read_fd)) 
-                        { 
-				if ((valread = read(sd, buffer, 1024))==0) 
-				{ 
-                                        printf("Host disconnected %s \n" ,itr2->second); 
-                                        close(sd); 
-					nicknames.erase(itr2); 
-                                	break;				
-				} 
-                                else
-				{ 
-	  				buffer[valread] = '\0';
-					for (auto itr1 = nicknames.begin(); itr1 != nicknames.end(); ++itr1)
-					dprintf(itr1->first,"%s says: %s\n",itr2->second, buffer);
-					
-                                 }  
-                        } 
-                } 
-        
-	          //  for (auto it3 = self->nicknames.begin(); it3!=self->nicknames.end();  ++it3)
-   // std::cout << it3->first << "->" << it3->second << std::endl; 
-
-	}
-        close(sd2);
-	//closing socket
-	close(sock);
-return this;
-//std::cout<<"ser"<<std::endl;
-}
-
-
-void Client::r()
-{
-
-        sock=socket(AF_INET, SOCK_STREAM, 0);
-        printf("client fd %i \n", sock);
-        connect(sock, (struct sockaddr*)&addres, sizeof(addres));
-        dprintf(sock, s);
-        for(;;)
-        {
-                memset(buffer, 0, sizeof(buffer));
-                FD_ZERO(&read_fd);
-                FD_SET(0, &read_fd);
-                FD_SET(sock, &read_fd);
- 		select(300, &read_fd, NULL, NULL, NULL);
-                if(FD_ISSET(0, &read_fd)){read(0, buffer,sizeof(buffer));dprintf(sock, buffer);}  
-                if(FD_ISSET(sock, &read_fd)){read(sock, buffer, sizeof(buffer));dprintf(0, buffer);}
-        }   
-        close(sock);
- 
-return this;
-//std::cout<<"cli"<<std::endl;
-}
-
 
 void connector( args *argz) {
 args* q=(args*)argz;
@@ -272,64 +177,64 @@ std::string a = "P6Client", b = typeid(peer).name();
 
 peer->sock=socket(AF_INET, SOCK_STREAM, 0);
 std::cout<< typeid(peer).name() << " fd " << peer->sock << std::endl;
-
-//printf("%c fd %i \n", b ,peer->sock);
-
-
-
 switch (a==b){
-
-        case true:
+	case true:
 	{
-	///       sock=socket(AF_INET, SOCK_STREAM, 0);
-        //printf("client fd %i \n", sock);
-        connect(sock, (struct sockaddr*)&addres, sizeof(addres));
-        dprintf(sock, s);
-        for(;;)
-        {
-                memset(buffer, 0, sizeof(buffer));
-                FD_ZERO(&read_fd);
-                FD_SET(0, &read_fd);
-                FD_SET(sock, &read_fd);
- 		select(300, &read_fd, NULL, NULL, NULL);
-                if(FD_ISSET(0, &read_fd)){read(0, buffer,sizeof(buffer));dprintf(sock, buffer);}  
-                if(FD_ISSET(sock, &read_fd)){read(sock, buffer, sizeof(buffer));dprintf(0, buffer);}
-        }   
-        close(sock);
+        connect(peer->sock, (struct sockaddr*)&peer->addres, sizeof(peer->addres));
+        dprintf(peer->sock, s);
 
-
-
-
-
-		break;
 	}
-        case false:
+	case false:
 	{
-		//std::cout<<c<<std::endl;
-	//socket creation
-        //sock=socket(AF_INET, SOCK_STREAM, 0);
-	//output socket file descriptor
-	//printf("server fd %i \n", sock);
-	//initialisation
-        bind(sock, (struct sockaddr*)&addres, sizeof(addres));
+
+	bind(sock, (struct sockaddr*)&addres, sizeof(addres));
 	//waiting for connection
         listen(sock, 300);
 
 	nicknames.insert({0,s});
 	
-        for(;;){
-        FD_ZERO(&read_fd);      
-        //comm->max_sd = comm->sock; 
-      //  for (auto it1 = self->nicknames.begin(); it1!=self->nicknames.end();  ++it1)
-    //std::cout << it1->first << "->" << it1->second << std::endl;  
-	for (auto itr = nicknames.begin(); itr != nicknames.end(); ++itr)FD_SET(itr->first, &read_fd);
-        FD_SET(sock, &read_fd);  
-	//wait for connection/recive message
-        select(300, &read_fd, NULL, NULL, NULL);
-	
+
+
+	}
+}
+
+
+
+
+for(;;)
+{
+
+		memset(buffer, 0, sizeof(buffer));
+                FD_ZERO(&read_fd);
+                FD_SET(0, &read_fd);
+                FD_SET(sock, &read_fd);
+
+
+if(a!=b)for (auto itr = nicknames.begin(); itr != nicknames.end(); ++itr)FD_SET(itr->first, &read_fd);
+
+ 		select(300, &read_fd, NULL, NULL, NULL);
+
+
+
+
+
+
+       if(a==b)if(FD_ISSET(0, &read_fd)){read(0, buffer,sizeof(buffer));dprintf(sock, buffer);}  
+               // if(FD_ISSET(sock, &read_fd)){read(sock, buffer, sizeof(buffer));dprintf(0, buffer);}
+
+
 	if (FD_ISSET(sock, &read_fd)) 
-                {	
-			//furst for scanner	
+	switch (a==b){
+
+        case true:
+	{
+         read(sock, buffer, sizeof(buffer));
+	 dprintf(0, buffer);
+		break;
+	}
+        case false:
+	{
+
 			new_socket = accept(sock,NULL,NULL);
 			//accepting connection
 			new_socket = accept(sock,NULL,NULL);
@@ -341,7 +246,13 @@ switch (a==b){
                 }
 		//for (auto it2 = self->nicknames.begin(); it2!=self->nicknames.end();  ++it2)
     //std::cout << it2->first << "->" << it2->second << std::endl; 
-		for (auto itr2 = nicknames.begin(); itr2 != nicknames.end(); ++itr2)
+
+	}//switch()
+
+
+
+
+	for (auto itr2 = nicknames.begin(); itr2 != nicknames.end(); ++itr2)
                 { 
 			sd = itr2->first; 
 			if (FD_ISSET(sd , &read_fd)) 
@@ -361,23 +272,14 @@ switch (a==b){
 					
                                  }  
                         } 
-                } 
-        
-	          //  for (auto it3 = self->nicknames.begin(); it3!=self->nicknames.end();  ++it3)
-   // std::cout << it3->first << "->" << it3->second << std::endl; 
-
-	}
-        close(sd2);
-	//closing socket
-	close(sock);
+             
+        	}
 
 
-		break;
-	}
+//for(;;)
 }
 
-//init(peer);
-//return peer;
+//run()
 }
 
 
