a07abb5dcec5296e08fcfa4279ee161b2d3de364
Denis Vergun <blu.eagle@mail.ru>
2023-03-04 15:24:34 +0500

    cli ser stream

---
+++ server.c
@@ -0,0 +1,100 @@
+// Server side C/C++ program to demonstrate Socket
+// programming
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+
+#include <arpa/inet.h>  //inet_addr define
+#include <stdio.h>  //printf()
+#include <sys/socket.h> //socket(), connect(), bind(), listen(), accept(), select(); 
+#include <unistd.h>  //close(), fread()
+#include <string>  //string type
+#include <map>
+#include <cstring>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <queue>
+#include <typeinfo>
+#include <ext/stdio_filebuf.h>
+#include <fstream>
+
+
+using namespace std;
+
+
+int main(int argc, char const* argv[])
+{
+	int server_fd, new_socket, valread;
+	struct sockaddr_in address;
+	int opt = 1;
+	int addrlen = sizeof(address);
+	char buffer[1024] = { 0 };
+	char hello = 'Hello from server';
+
+	// Creating socket file descriptor
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+
+	// Forcefully attaching socket to the port 8080
+	if (setsockopt(server_fd, SOL_SOCKET,
+				SO_REUSEADDR | SO_REUSEPORT, &opt,
+				sizeof(opt))) {
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
+
+	// Forcefully attaching socket to the port 8080
+	if (bind(server_fd, (struct sockaddr*)&address,
+			sizeof(address))
+		< 0) {
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	if (listen(server_fd, 3) < 0) {
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+	if ((new_socket
+		= accept(server_fd, (struct sockaddr*)&address,
+				(socklen_t*)&addrlen))
+		< 0) {
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+
+	
+
+
+	valread = read(new_socket, buffer, 1024);
+	printf("%s\n", buffer);
+	int posix_handle = new_socket;//fileno(::fopen("test.txt", "r"));
+
+    __gnu_cxx::stdio_filebuf<char> filebuf(posix_handle, std::ios::out); // 1
+    ostream is(&filebuf); // 2
+
+    //string line;
+    //getline(is, line);
+
+	//cout << line<<endl;
+	//send(new_socket, hello, strlen(hello), 0);
+	is << "hello" << endl;	
+    printf("Hello message sent\n");
+
+	// closing the connected socket
+	close(new_socket);
+	// closing the listening socket
+	shutdown(server_fd, SHUT_RDWR);
+	return 0;
+}
+
+
