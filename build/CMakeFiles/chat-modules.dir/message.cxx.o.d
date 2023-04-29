dee0e65cbfc709eaba3b511040f4cd31beb29e28
Denis Vergun <blu.eagle@mail.ru>
2023-04-29 18:07:32 +0500

    pro

--- build/CMakeFiles/chat-modules.dir/message.cxx.o.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/chat-modules.dir/message.cxx.o: \
- /home/programmer/chat/message.cxx /usr/include/stdc-predef.h
-CMakeFiles/chat-modules.dir/message.cxx.o: \
- /usr/include/c++/13.0.1/iostream.c++m server.c++m client.c++m \
- ipscan.c++m
-CXX_IMPORTS += /usr/include/c++/13.0.1/iostream.c++m server.c++m \
- client.c++m ipscan.c++m
--- build/CMakeFiles/std_modules.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/std_modules"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/std_modules.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
