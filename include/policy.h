4df7b029b997b5ee529cb0a4dcac090ddeb3d75e
Denis Vergun <blu.eagle@mail.ru>
2023-11-12 11:33:27 +0500

    output policy message client

--- include/policy.h
+++ include/policy.h
@@ -25,7 +25,7 @@ TCPClientPolicy();
 void createSocket(Client *client)const override{
         client->sock=socket(AF_INET, SOCK_STREAM, 0);
         if(client->sock)
-        std::cout << typeid(client).name() << " fd " << client->sock << std::endl;
+        std::cout <<"TCP " <<typeid(client).name() << " fd " << client->sock << std::endl;
         else
         std::cout << "creation socket error" << std::endl;
 }
@@ -75,7 +75,7 @@ UDPClientPolicy();
 void createSocket(Client *client)const override{ 
 	client->sock=socket(AF_INET, SOCK_DGRAM, 0);
         if(client->sock)
-        std::cout << typeid(client).name() << " fd " << client->sock << std::endl;
+        std::cout <<"UDP " <<typeid(client).name() << " fd " << client->sock << std::endl;
         else
         std::cout << "creation socket error" << std::endl;
 
