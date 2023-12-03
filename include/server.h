db386c28190967b0abae386e78722ae26c448218
Denis Vergun <blu.eagle@mail.ru>
2023-12-03 19:49:37 +0500

    server.h define

--- include/server.h
+++ include/server.h
@@ -84,11 +84,112 @@ void createSocket(Server *server)const override{
 void connectInit()const override{
         std::cout << "no ip addres specified" << std::endl;
 }
-void connectInit(const char* addr)const override{}
-void selinit(Server *server)const override{}
-        void sel()const override{};///<\param void  \return void
-        void conn()const override{};///<\param void  \return void
-        void sendmes()const override{};///<\param void  \return void
+void connectInit(const char* addr)const override{
+
+	addres.sin_family = AF_INET;
+        addres.sin_port = htons(PORT);
+        addres.sin_addr.s_addr = htonl(INADDR_ANY);
+
+        bind(sock, (struct sockaddr*)&addres, sizeof(addres));
+        //waiting for connection
+        listen(sock, 300);
+        //std::cout <<"test" <<std::endl;
+        nicknames.push_back({0,"test"});
+        return this;
+}
+
+void selinit(Server *server)const override{
+              memset(buffer, 0, sizeof(buffer));
+                FD_ZERO(&read_fd);
+                FD_SET(0, &read_fd);
+                FD_SET(sock, &read_fd);
+
+}
+        void sel()const override{
+for (auto itr = nicknames.begin(); itr != nicknames.end(); ++itr)
+FD_SET(itr->first, &read_fd);
+//                select(300, &peer->read_fd, NULL, NULL, NULL);
+
+return this;
+
+};///<\param void  \return void
+        void conn()const override{
+                        //peer->new_socket = accept(peer->sock,NULL,NULL);
+                        //accepting connection
+                        new_socket = accept(sock,NULL,NULL);
+                        //char g[80]={"sos"};
+                        //g="sos";
+                        //read(peer->new_socket,g,1024);
+                        nicknames.push_back({new_socket,"test"});
+                        std::cout<< "New connection "<<new_socket<<std::endl;
+                        //printf("New connection %s\n", peer->new_socket);
+                        dprintf(new_socket,"welcome %d\n", new_socket);  
+
+};///<\param void  \return void
+        void sendmes()const override{
+
+
+
+	for (auto itr2 = nicknames.begin(); itr2 != nicknames.end(); ++itr2)
+                { 
+			sd = itr2->first; 
+			if (FD_ISSET(sd , &read_fd)) 
+                        { 
+				if ((valread = read(sd, buffer, 1024))) 
+				/*{ 
+                                        printf("Host disconnected %s \n" ,itr2->second); 
+                                        close(peer->sd); 
+					peer->nicknames.erase(itr2); 
+                                	break;				
+				} 
+                                else
+					*/
+				{ 
+	  				buffer[valread] = '\0';
+					for (auto itr1 = nicknames.begin(); itr1 != nicknames.end(); ++itr1)
+					dprintf(itr1->first,"%s says: %s\n",itr2->second, buffer);
+					
+                                 }  
+                        } 
+             
+        	}
+
+
+
+};///<\param void  \return void
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 };
